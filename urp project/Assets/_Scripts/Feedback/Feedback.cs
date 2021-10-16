40dd2dd432bfbe0e0d7667f49458630efb331894
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2021-10-16 09:28:24 +0700

    create flash light feedback :flashlight:

--- urp project/Assets/_Scripts/Feedback/Feedback.cs
+++ urp project/Assets/_Scripts/Feedback/Feedback.cs
@@ -4,4 +4,9 @@ public abstract class Feedback : MonoBehaviour
 {
     public abstract void CreateFeedback();
     public abstract void CompletePreviousFeedback();
+
+    protected virtual void OnDestroy()
+    {
+        CompletePreviousFeedback();
+    }
 }
---
+++ urp project/Assets/_Scripts/Feedback/FlashLightFeedback.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class FlashLightFeedback : Feedback
+{
+    [SerializeField] private Light2D lightTarget;
+    [SerializeField] private float lightOnDelay = 0.01f;
+    [SerializeField] private float lightOffDelay = 0.01f;
+    [SerializeField] private bool defaultState = false;
+
+    public override void CreateFeedback()
+    {
+        StartCoroutine(ToggleLight(lightOnDelay, true, () => StartCoroutine(ToggleLight(lightOffDelay, false))));
+    }
+
+    public override void CompletePreviousFeedback()
+    {
+        StopAllCoroutines();
+        lightTarget.enabled = defaultState;
+
+    }
+
+    private IEnumerator ToggleLight(float time, bool result, Action finishCallback = null)
+    {
+        yield return new WaitForSeconds(time);
+        lightTarget.enabled = result;
+        finishCallback?.Invoke();
+    }
+}
